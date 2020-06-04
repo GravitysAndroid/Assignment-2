40f6796332c74fa38fccfe177bed8c125c6d5cde
Hayden <haydenwill95@gmail.com>
2020-06-04 14:52:51 +1200

    Started ClsTour and ClsCompanyData

--- Tourism Business/Assignment Two/ClsCompanyData.cs
+++ Tourism Business/Assignment Two/ClsCompanyData.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Assignment_Two
 {
+    [Serializable]
     public class ClsCompanyData
     {
+        private static string fileName = "BusinessStorage";
+        private static Dictionary<string, ClsTour> _TourList = new Dictionary<string, ClsTour>();
+
+        public static Dictionary<string, ClsTour> TourList
+        {
+            get => _TourList; /*set => _TourList = value;*/
+        }
+
         public void Save()
         {
-            throw new System.Exception("Not implemented");
+            using (FileStream lcFileStream = new FileStream(fileName, FileMode.Create))
+            {
+                BinaryFormatter lcFormatter = new BinaryFormatter();
+                lcFormatter.Serialize(lcFileStream, _TourList);
+            }
         }
         public void Retreive()
         {
-            throw new System.Exception("Not implemented");
+            using (FileStream lcFileStream = new FileStream(fileName, FileMode.Open))
+            {
+                BinaryFormatter lcFormatter = new BinaryFormatter();
+                _TourList = (Dictionary<string, ClsTour>)lcFormatter.Deserialize(lcFileStream);
+            }
         }
 
         private System.Collections.Generic.List<ClsTour> _TourList;
--- Tourism Business/Assignment Two/ClsTour.cs
+++ Tourism Business/Assignment Two/ClsTour.cs
@@ -1,106 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Assignment_Two
 {
+    [Serializable]
     public class ClsTour
     {
         private String _code;
         public String Code
         {
-            get
-            {
-                return _code;
-            }
-            set
-            {
-                _code = value;
-            }
+            get { return _code; }
+            set { _code = value; }
         }
+
         private String _name;
         public String Name
         {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                _name = value;
-            }
+            get { return _name; }
+            set { _name = value; }
         }
+
         private DateTime _startDate = DateTime.Today;
         public DateTime StartDate
         {
-            get
-            {
-                return _startDate;
-            }
-            set
-            {
-                _startDate = value;
-            }
+            get { return _startDate; }
+            set { _startDate = value; }
         }
+
         private DateTime _endDate = DateTime.Today;
         public DateTime EndDate
         {
-            get
-            {
-                return _endDate;
-            }
-            set
-            {
-                _endDate = value;
-            }
+            get { return _endDate; }
+            set { _endDate = value; }
         }
+
         private int _maxPax;
         public int MaxPax
         {
-            get
-            {
-                return _maxPax;
-            }
-            set
-            {
-                _maxPax = value;
-            }
+            get { return _maxPax; }
+            set { _maxPax = value; }
         }
+
         private int _distance;
         public int Distance
         {
-            get
-            {
-                return _distance;
-            }
-            set
-            {
-                _distance = value;
-            }
+            get { return _distance; }
+            set { _distance = value; }
         }
+
         private int _markUp;
         public int MarkUp
         {
-            get
-            {
-                return _markUp;
-            }
-            set
-            {
-                _markUp = value;
-            }
+            get { return _markUp; }
+            set { _markUp = value; }
+        }
+
+        public static ClsTour NewTour(int prChoice)
+        {
+            if (prChoice == 0)
+                return new ClsMOEStudent();
+            else
+                return new ClsInternationalStudent();
         }
 
         public String ToString()
         {
             throw new System.Exception("Not implemented");
         }
+
         public Decimal CalculatePricePassenger()
         {
-            throw new System.Exception("Not implemented");
+            decimal lcTotal = 0;
+            foreach (ClsTour lcCost in TourList.Values)
+                lcTotal += lcCost.Balance;
+            return lcTotal;
         }
+
         public void ViewEdit()
         {
             throw new System.Exception("Not implemented");
